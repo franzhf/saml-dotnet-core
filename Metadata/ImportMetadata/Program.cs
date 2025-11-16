26ed030a2f106f8b25e96b73ae2993c3a1878f1d
Franz Flores <franz.flores@meta-dao.com>
2025-11-16 11:20:39 -0400

    add the poc of saml .net core

---
+++ Metadata/ImportMetadata/Program.cs
@@ -0,0 +1,52 @@
+﻿using ComponentSpace.Saml2.Configuration;
+using ComponentSpace.Saml2.Configuration.Serialization;
+using ComponentSpace.Saml2.Metadata.Import;
+using System;
+using System.IO;
+
+namespace ImportMetadata
+{
+    /// <summary>
+    /// Imports SAML metadata into the SAML configuration.
+    /// 
+    /// Usage: dotnet ImportMetadata.dll
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("SAML metadata file or URL to import: ");
+                var metadataLocation = Console.ReadLine();
+
+                var metadataToConfiguration = new MetadataToConfiguration();
+
+                var samlConfigurations = metadataLocation.StartsWith("http", StringComparison.OrdinalIgnoreCase)
+                    ? metadataToConfiguration.ImportUrlAsync(metadataLocation).Result
+                    : metadataToConfiguration.ImportFile(metadataLocation);
+
+                SaveConfiguration(samlConfigurations);
+            }
+
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+        }
+
+        private static void SaveConfiguration(SamlConfigurations samlConfigurations)
+        {
+            Console.Write("SAML configuration file [saml.json]: ");
+
+            var fileName = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = "saml.json";
+            }
+
+            File.WriteAllText(fileName, ConfigurationSerializer.Serialize(samlConfigurations));
+        }
+    }
+}
